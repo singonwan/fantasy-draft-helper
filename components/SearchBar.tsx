d85782ac0cb748149652bfb0ac47b03b0d4ff2dc
singonwan <singonwan@gmail.com>
2024-08-14 23:56:11 -0400

    adding label to position listbox

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,6 +4,8 @@ import PlayerTable from './PlayerTable';
 import { PLAYERS } from '@/data/players';
 import { Player } from '@/types';
 import {
+	Field,
+	Label,
 	Listbox,
 	ListboxButton,
 	ListboxOption,
@@ -82,41 +84,44 @@ const SearchBar = () => {
 					Player Search
 				</label>
 				<div className="mx-auto w-40 py-4">
-					<Listbox value={positionSelected} onChange={setPositionSelected}>
-						<ListboxButton
-							className={clsx(
-								'relative block w-full rounded-lg bg-black/5 py-1.5 pr-8 pl-3 text-left text-sm/6 text-black',
-								'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-							)}
-						>
-							{positionSelected.position}
-							<ChevronDownIcon
-								className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-black/60"
-								aria-hidden="true"
-							/>
-						</ListboxButton>
-						<ListboxOptions
-							anchor="bottom"
-							transition
-							className={clsx(
-								'w-[var(--button-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none',
-								'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
-							)}
-						>
-							{positions.map((position) => (
-								<ListboxOption
-									key={position.position}
-									value={position}
-									className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
-								>
-									<CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-									<div className="text-sm/6 text-black">
-										{position.position}
-									</div>
-								</ListboxOption>
-							))}
-						</ListboxOptions>
-					</Listbox>
+					<Field>
+						<Label>Position</Label>
+						<Listbox value={positionSelected} onChange={setPositionSelected}>
+							<ListboxButton
+								className={clsx(
+									'relative block w-full rounded-lg bg-black/5 py-1.5 pr-8 pl-3 text-left text-sm/6 text-black',
+									'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
+								)}
+							>
+								{positionSelected.position}
+								<ChevronDownIcon
+									className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-black/60"
+									aria-hidden="true"
+								/>
+							</ListboxButton>
+							<ListboxOptions
+								anchor="bottom"
+								transition
+								className={clsx(
+									'w-[var(--button-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none',
+									'transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0'
+								)}
+							>
+								{positions.map((position) => (
+									<ListboxOption
+										key={position.position}
+										value={position}
+										className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+									>
+										<CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
+										<div className="text-sm/6 text-black">
+											{position.position}
+										</div>
+									</ListboxOption>
+								))}
+							</ListboxOptions>
+						</Listbox>
+					</Field>
 				</div>
 				<div className="flex items-center justify-center w-full flex-wrap relative">
 					<input
