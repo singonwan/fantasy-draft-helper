eecad5a59fbbf4ad5619179fd41f1f38e44848ff
singonwan <singonwan@gmail.com>
2024-08-21 15:53:16 -0400

    changed button on navbars to next links

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -12,18 +12,18 @@ const NavBar = () => {
 					Fantasy Draft Helper
 				</Link>
 				<div className="flex items-center gap-x-1">
-					<button
+					<Link
 						className="hidden px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
-						type="button"
+						href="/login"
 					>
 						<span>Log In</span>
-					</button>
-					<button
+					</Link>
+					<Link
 						className="hidden select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block"
-						type="button"
+						href="/signup"
 					>
 						<span>Sign up</span>
-					</button>
+					</Link>
 				</div>
 			</div>
 		</nav>
