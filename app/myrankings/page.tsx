6ffdbea12efb5d9878f7e337aee4deef4ef4e9e6
singonwan <singonwan@gmail.com>
2024-08-21 15:16:38 -0400

    added signup and login page, and moved background to root layout

--- app/myrankings/page.tsx
+++ app/myrankings/page.tsx
@@ -4,34 +4,20 @@ import React from 'react';
 
 const MyOrder = () => {
 	return (
-		<div className="bg-white min-h-screen">
-			<div className="relative isolate px-6 pt-14 lg:px-8">
-				<div
-					aria-hidden="true"
-					className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+		<>
+			<div className="px-24">
+				<Link
+					href="/"
+					className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
 				>
-					<div
-						style={{
-							clipPath:
-								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-						}}
-						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-					/>
-				</div>
-				<div className="px-24">
-					<Link
-						href="/"
-						className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-					>
-						Back
-					</Link>
-				</div>
-				<h1 className="font-bold text-3xl p-8 uppercase text-center">
-					Fantasy Draft Helper
-				</h1>
-				<SearchBar />
+					Back
+				</Link>
 			</div>
-		</div>
+			<h1 className="font-bold text-3xl p-8 uppercase text-center">
+				Fantasy Draft Helper
+			</h1>
+			<SearchBar />
+		</>
 	);
 };
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,57 +2,32 @@ import Link from 'next/link';
 
 export default function Hero() {
 	return (
-		<div className="bg-white min-h-screen">
-			<div className="relative isolate px-6 pt-14 lg:px-8">
-				<div
-					aria-hidden="true"
-					className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-				>
-					<div
-						style={{
-							clipPath:
-								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-						}}
-						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-					/>
-				</div>
-				<div className="mx-auto max-w-xl py-32 sm:py-48 lg:py-56">
-					<div className="text-center">
-						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-							Use Fantasy Draft Helper to set your{' '}
-							<span className="text-indigo-700">own rankings</span>
-						</h1>
-						<p className="mt-6 text-lg leading-8 text-gray-600">
-							set your rankings, with the ability to drag and drop to adjust
-							with ease.
-						</p>
-						<div className="mt-10 flex items-center justify-center gap-x-6">
-							<Link
-								href="/myrankings"
-								className="rounded-md bg-indigo-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-							>
-								Your Rankings
-							</Link>
-							{/* <a
-								href="#"
-								className="text-sm font-semibold leading-6 text-gray-900"
-							>
-								Learn more <span aria-hidden="true">→</span>
-							</a> */}
-						</div>
-					</div>
-				</div>
-				<div
-					aria-hidden="true"
-					className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-				>
-					<div
-						style={{
-							clipPath:
-								'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-						}}
-						className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-					/>
+		<div className="mx-auto max-w-xl py-32 sm:py-48 lg:py-56">
+			<div className="text-center">
+				<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+					Use Fantasy Draft Helper to set your{' '}
+					<span className="text-indigo-700">own rankings</span>
+				</h1>
+				<p className="mt-6 text-lg leading-8 text-gray-600">
+					set your rankings, with the ability to drag and drop to adjust with
+					ease.
+				</p>
+				<p className="mt-6 text-lg leading-8 text-gray-600">
+					Create an account to save these rankings.
+				</p>
+				<div className="mt-10 flex items-center justify-center gap-x-6">
+					<Link
+						href="/myrankings"
+						className="rounded-md bg-indigo-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						Your Rankings
+					</Link>
+					<Link
+						href="/login"
+						className="text-sm font-semibold leading-6 text-gray-900"
+					>
+						Log in<span aria-hidden="true">→</span>
+					</Link>
 				</div>
 			</div>
 		</div>
