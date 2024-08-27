3599d1f3abd00ff29f12f217ae9f00becda41513
singonwan <singonwan@gmail.com>
2024-08-27 16:52:27 -0400

    udpated zod definition for signupform schema to include confirmpassword

--- app/_lib/definitions.ts
+++ app/_lib/definitions.ts
@@ -1,7 +1,15 @@
 import { z } from 'zod';
 
-export const signupFormSchema = z.object({
-	name: z.string().min(1, 'Name is required'),
-	email: z.string().email('Invalid email address'),
-	password: z.string().min(6, 'Password must be at least 6 characters long'),
-});
+export const signupFormSchema = z
+	.object({
+		name: z.string().min(1, 'Name is required'),
+		email: z.string().email('Invalid email address'),
+		password: z.string().min(6, 'Password must be at least 6 characters long'),
+		confirmPassword: z
+			.string()
+			.min(6, 'Password must be at least 6 characters long'),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ['confirmPassword'], //setting path of error msg to 'confirm password'
+		message: 'passwords do not match',
+	});
